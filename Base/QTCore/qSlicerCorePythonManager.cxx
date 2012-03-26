pieper/SlicerGitSVNArchive

commit a413c45e5cd970e321032a026a2f149df352700a

BUG: Ensure python stderr are not redirected into stdout
git-svn-id: http://svn.slicer.org/Slicer4/trunk@19700 3bd1e089-480b-0410-8dfb-8563597acbee

Base/QTCore/qSlicerCorePythonManager.cxx
@@ -33,7 +33,10 @@
 //-----------------------------------------------------------------------------
 qSlicerCorePythonManager::qSlicerCorePythonManager(QObject* _parent) : Superclass(_parent)
 {
-  this->setInitializationFlags(PythonQt::RedirectStdOut);
+  int flags = this->initializationFlags();
+  flags &= ~(PythonQt::RedirectStdOut); // Clear bit
+  flags &= ~(PythonQt::IgnoreSiteModule); // Clear bit
+  this->setInitializationFlags(flags);
 }
 
 //-----------------------------------------------------------------------------

commit 58f8f4fd23bf3dd80378ec0dd788c90e5ede34b9

BUG: Display result of python command into the qt console.
* This fix a regression introduced in r19700

git-svn-id: http://svn.slicer.org/Slicer4/trunk@19709 3bd1e089-480b-0410-8dfb-8563597acbee

Base/QTCore/qSlicerCorePythonManager.cxx
@@ -37,7 +37,6 @@
 qSlicerCorePythonManager::qSlicerCorePythonManager(QObject* _parent) : Superclass(_parent)
 {
   int flags = this->initializationFlags();
-  flags &= ~(PythonQt::RedirectStdOut); // Clear bit
   flags &= ~(PythonQt::IgnoreSiteModule); // Clear bit
   this->setInitializationFlags(flags);
 }