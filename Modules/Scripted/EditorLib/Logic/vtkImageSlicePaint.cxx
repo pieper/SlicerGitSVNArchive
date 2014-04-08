3e0762265cd37aa063f10ebf007b311db63b4dac
Julien <julien.finet@kitware.com>
2014-04-08 16:57:15 -0400

    BUG: Fix vtkImageSlicePaint TopRight initialization

--- Modules/Scripted/EditorLib/Logic/vtkImageSlicePaint.cxx
+++ Modules/Scripted/EditorLib/Logic/vtkImageSlicePaint.cxx
@@ -25,9 +25,9 @@ vtkStandardNewMacro(vtkImageSlicePaint);
 //----------------------------------------------------------------------------
 vtkImageSlicePaint::vtkImageSlicePaint()
 {
-  for (int i = 0; i < 3; i++)
+  for (size_t i = 0; i < 3; ++i)
     {
-    this->TopLeft[i] = this->TopLeft[i] = this->BottomLeft[i] = this->BottomRight[i] = 0;
+    this->TopLeft[i] = this->TopRight[i] = this->BottomLeft[i] = this->BottomRight[i] = 0;
     }
 
   this->MaskImage = NULL;
