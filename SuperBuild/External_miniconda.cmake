aabf6311d603c08d812a975c7bb56a1e9b954831
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2015-08-24 15:45:49 -0400

    COMP: Add missing code for setting launcher and python CMake variables
    *** WIP ***
    Still need to refactor code setting python launcher settings so that it is
    common between miniconda and python
    *** WIP ***

--- SuperBuild/External_miniconda.cmake
+++ SuperBuild/External_miniconda.cmake
@@ -1,5 +1,8 @@
 
 set(proj miniconda)
+if(NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_python)
+  list(APPEND ${proj}_DEPENDENCIES CTKAPPLAUNCHER)
+endif()
 
 # Include dependent projects if any
 ExternalProject_Include_Dependencies(${proj} PROJECT_VAR proj DEPENDS_VAR ${proj}_DEPENDENCIES)
@@ -12,7 +15,7 @@ if(NOT DEFINED PYTHON_INCLUDE_DIR
   OR NOT DEFINED PYTHON_LIBRARY
   OR NOT DEFINED PYTHON_EXECUTABLE)
 
-  set (python_DIR ${CMAKE_BINARY_DIR}/python-conda)
+  set(python_DIR ${CMAKE_BINARY_DIR}/${proj})
   file(MAKE_DIRECTORY ${python_DIR})
 
   # Choose install file depending on the platform version
@@ -54,22 +57,147 @@ if(NOT DEFINED PYTHON_INCLUDE_DIR
     )
   endif()
 
-  # Test: install numpy (requires to disable Slicer Numpy).... OK
-  ExternalProject_Add_Step(${proj} installNumpy
+  if(UNIX)
+    set(python_IMPORT_SUFFIX so)
+    if(APPLE)
+      set(python_IMPORT_SUFFIX dylib)
+    endif()
+    set(slicer_PYTHON_SHARED_LIBRARY_DIR ${python_DIR}/lib)
+    set(PYTHON_INCLUDE_DIR ${python_DIR}/include/python2.7)
+    set(PYTHON_LIBRARY ${python_DIR}/lib/libpython2.7.${python_IMPORT_SUFFIX})
+    set(PYTHON_EXECUTABLE ${python_DIR}/bin/SlicerPython)
+    set(slicer_PYTHON_REAL_EXECUTABLE ${python_DIR}/bin/python)
+  elseif(WIN32)
+    set(slicer_PYTHON_SHARED_LIBRARY_DIR ${python_DIR}/bin)
+    set(PYTHON_INCLUDE_DIR ${python_DIR}/include)
+    set(PYTHON_LIBRARY ${python_DIR}/libs/python27.lib)
+    set(PYTHON_EXECUTABLE ${python_DIR}/bin/SlicerPython.exe)
+    set(slicer_PYTHON_REAL_EXECUTABLE ${python_DIR}/bin/python.exe)
+  else()
+    message(FATAL_ERROR "Unknown system !")
+  endif()
+
+    if(NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_python)
+
+    # Configure python launcher
+    configure_file(
+      SuperBuild/python_customPython_configure.cmake.in
+      ${CMAKE_CURRENT_BINARY_DIR}/python_customPython_configure.cmake
+      @ONLY)
+    set(python_customPython_configure_args)
+
+    if(PYTHON_ENABLE_SSL)
+      set(python_customPython_configure_args -DOPENSSL_EXPORT_LIBRARY_DIR:PATH=${OPENSSL_EXPORT_LIBRARY_DIR})
+    endif()
+
+    ExternalProject_Add_Step(${proj} python_customPython_configure
+      COMMAND ${CMAKE_COMMAND} ${python_customPython_configure_args}
+        -P ${CMAKE_CURRENT_BINARY_DIR}/python_customPython_configure.cmake
+      DEPENDEES install
+      )
+
+  endif()
+
+  if(CMAKE_CONFIGURATION_TYPES)
+    set(CMAKE_CFG_INTDIR ${SAVED_CMAKE_CFG_INTDIR}) # Restore CMAKE_CFG_INTDIR
+  endif()
+
+  #-----------------------------------------------------------------------------
+  # Launcher setting specific to build tree
+
+  set(_lib_subdir lib)
+  if(WIN32)
+    set(_lib_subdir bin)
+  endif()
+
+  # library paths
+  set(${proj}_LIBRARY_PATHS_LAUNCHER_BUILD ${python_DIR}/${_lib_subdir})
+  mark_as_superbuild(
+    VARS ${proj}_LIBRARY_PATHS_LAUNCHER_BUILD
+    LABELS "LIBRARY_PATHS_LAUNCHER_BUILD"
+    )
+
+  # paths
+  set(${proj}_PATHS_LAUNCHER_BUILD ${python_DIR}/bin)
+  mark_as_superbuild(
+    VARS ${proj}_PATHS_LAUNCHER_BUILD
+    LABELS "PATHS_LAUNCHER_BUILD"
+    )
+
+  # pythonpath
+  set(_pythonhome ${CMAKE_BINARY_DIR}/python-install)
+  set(pythonpath_subdir lib/python2.7)
+  if(CMAKE_SYSTEM_NAME STREQUAL "Windows")
+    set(pythonpath_subdir Lib)
+  endif()
+
+  set(${proj}_PYTHONPATH_LAUNCHER_BUILD
+    ${_pythonhome}/${pythonpath_subdir}
+    ${_pythonhome}/${pythonpath_subdir}/lib-dynload
+    ${_pythonhome}/${pythonpath_subdir}/site-packages
+    )
+  mark_as_superbuild(
+    VARS ${proj}_PYTHONPATH_LAUNCHER_BUILD
+    LABELS "PYTHONPATH_LAUNCHER_BUILD"
+    )
+
+  # environment variables
+  set(${proj}_ENVVARS_LAUNCHER_BUILD "PYTHONHOME=${python_DIR}")
+  mark_as_superbuild(
+    VARS ${proj}_ENVVARS_LAUNCHER_BUILD
+    LABELS "ENVVARS_LAUNCHER_BUILD"
+    )
+
+  #-----------------------------------------------------------------------------
+  # Launcher setting specific to install tree
+
+  # library paths
+  if(UNIX)
+    # On windows, python libraries are installed along with the executable
+    set(${proj}_LIBRARY_PATHS_LAUNCHER_INSTALLED <APPLAUNCHER_DIR>/lib/Python/lib)
+    mark_as_superbuild(
+      VARS ${proj}_LIBRARY_PATHS_LAUNCHER_INSTALLED
+      LABELS "LIBRARY_PATHS_LAUNCHER_INSTALLED"
+      )
+  endif()
+
+  # pythonpath
+  set(${proj}_PYTHONPATH_LAUNCHER_INSTALLED
+    <APPLAUNCHER_DIR>/lib/Python/${pythonpath_subdir}
+    <APPLAUNCHER_DIR>/lib/Python/${pythonpath_subdir}/lib-dynload
+    <APPLAUNCHER_DIR>/lib/Python/${pythonpath_subdir}/site-packages
+    )
+  mark_as_superbuild(
+    VARS ${proj}_PYTHONPATH_LAUNCHER_INSTALLED
+    LABELS "PYTHONPATH_LAUNCHER_INSTALLED"
+    )
+
+  # environment variables
+  set(${proj}_ENVVARS_LAUNCHER_INSTALLED "PYTHONHOME=<APPLAUNCHER_DIR>/lib/Python")
+  mark_as_superbuild(
+    VARS ${proj}_ENVVARS_LAUNCHER_INSTALLED
+    LABELS "ENVVARS_LAUNCHER_INSTALLED"
+    )
+
+  #-----------------------------------------------------------------------------
+  # numpy
+  ExternalProject_Add_Step(${proj} ${proj}-install-numpy
     COMMAND ${python_DIR}/bin/conda install --yes --quiet numpy
     DEPENDEES install
     )
 
   # Test: install scipy... OK
-  # ExternalProject_Add_Step(${proj} installSciPy
+  # ExternalProject_Add_Step(${proj} ${proj}-install-scipy
   #   COMMAND ${python_DIR}/bin/conda install --yes --quiet scipy
-  #   DEPENDEES installNumpy
+  #   DEPENDEES ${proj}-install-numpy
   #   )
 
-  ExternalProject_Add_Step(${proj} installSciPy
+  # scipy
+  ExternalProject_Add_Step(${proj} ${proj}-install-scikit-learn
     COMMAND ${python_DIR}/bin/conda install --yes --quiet scikit-learn
-    DEPENDEES installNumpy
+    DEPENDEES ${proj}-install-numpy
     )
+  #-----------------------------------------------------------------------------
 
 else()
   ExternalProject_Add_Empty(${proj} DEPENDS ${${proj}_DEPENDENCIES})
@@ -86,3 +214,9 @@ mark_as_superbuild(
 ExternalProject_Message(${proj} "PYTHON_EXECUTABLE:${PYTHON_EXECUTABLE}")
 ExternalProject_Message(${proj} "PYTHON_INCLUDE_DIR:${PYTHON_INCLUDE_DIR}")
 ExternalProject_Message(${proj} "PYTHON_LIBRARY:${PYTHON_LIBRARY}")
+
+if(WIN32)
+  set(PYTHON_DEBUG_LIBRARY ${PYTHON_LIBRARY})
+  mark_as_superbuild(VARS PYTHON_DEBUG_LIBRARY LABELS "FIND_PACKAGE")
+  ExternalProject_Message(${proj} "PYTHON_DEBUG_LIBRARY:${PYTHON_DEBUG_LIBRARY}")
+endif()
