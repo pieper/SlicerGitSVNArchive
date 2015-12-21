0137e394670078b9beb0ddc0383f0d731588be90
Steve Pieper <pieper@isomics.com>
2015-12-21 12:51:46 -0500

    ENH: add interpolation option for texture image
    Allows nearest neighbor texture access for categorical data volumes
    (i.e. label maps)

--- Libs/vtkAddon/vtkOpenGLTextureImage.cxx
+++ Libs/vtkAddon/vtkOpenGLTextureImage.cxx
@@ -38,6 +38,7 @@ vtkOpenGLTextureImage::vtkOpenGLTextureImage()
   this->ShaderComputation = NULL;
   this->ImageData = NULL;
   this->TextureName = 0;
+  this->Interpolate = 1;
   this->TextureMTime = 0;
 }
 
@@ -128,8 +129,16 @@ bool vtkOpenGLTextureImage::UpdateTexture()
 
   glGenTextures(1, &(this->TextureName));
   glBindTexture(GL_TEXTURE_3D, this->TextureName);
-  glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  if (this->Interpolate)
+    {
+    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    }
+  else
+    {
+    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    }
   glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
   glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
   glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
